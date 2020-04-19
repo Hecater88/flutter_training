586a137c60bcab14a439ee8e938423566ad18d69
hecater88 <virgilionayal@gmail.com>
2020-04-19 19:48:10 +0200

    Second commit

--- nerfli/lib/main.dart
+++ nerfli/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Películas',
+      title: 'Películass',
       initialRoute: '/',
       routes: {
         '/':(BuildContext context) => HomePage()
